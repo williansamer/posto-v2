4f15059194314998cce58b010ed03588c8dc9103
Willian Samer de Oliveira <willian.samer@gmail.com>
2021-07-28 07:34:24 -0300

    up criado o postos.js

---
+++ postos.js
@@ -0,0 +1,13 @@
+let postos = [{
+              codigo: 001,
+              nome: "Auto Posto Avenida",
+              bandeira: "",
+              endereco: "Avenida Emílio Arroyo Hernandes, 2337 - Fone: (17) 3421-7551",
+              horario: "",
+              etanol: "",
+              g_comum: "",
+              g_aditivada: "",
+              d_500: "",
+              d_s10: "",
+              conveniencia: true,
+              site: true}]
